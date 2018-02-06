ce7a91420da8e2c255ba1c743f8b2bd108d9d26c
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-06 23:46:52 +0500

    PDO adapter

--- Adapter/PDO.php
+++ Adapter/PDO.php
@@ -14,11 +14,44 @@ declare(strict_types=1);
 
 namespace Comely\IO\Database\Adapter;
 
+use Comely\IO\Database\Exception\ConnectionException;
+use Comely\IO\Database\Queries\Query;
+
 /**
  * Class PDO
  * @package Comely\IO\Database\Adapter
  */
-class PDO
+abstract class PDO
 {
+    /** @var \PDO */
+    protected $pdo;
 
+    /**
+     * PDO constructor.
+     * @param ServerCredentials $server
+     * @throws ConnectionException
+     */
+    public function __construct(ServerCredentials $server)
+    {
+        $options = [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION];
+        if ($server->persistent === true) {
+            $options[\PDO::ATTR_PERSISTENT] = true;
+        }
+
+        try {
+            $this->pdo = new \PDO($server->dsn, $server->username, $server->password, $options);
+        } catch (\PDOException $e) {
+            throw new ConnectionException($e->getMessage(), $e->getCode());
+        }
+    }
+
+    /**
+     * @param int $type
+     * @param Query $query
+     * @return mixed
+     */
+    protected function run(int $type, Query $query)
+    {
+
+    }
 }
